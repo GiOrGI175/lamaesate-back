bc0d6030cb4468e492021a01c69dfba40b54705c
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2025-10-27 23:58:37 +0400

    feature/socket

--- app.js
+++ app.js
@@ -7,8 +7,57 @@ import chatRoute from './routes/chat.route.js';
 import messageRoute from './routes/message.route.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import http from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+    credentials: true,
+  },
+});
+
+let onlineUser = [];
+const addUser = (userId, socketId) => {
+  const userExits = onlineUser.find((user) => user.userId === userId);
+
+  if (!userExits) {
+    onlineUser.push({ userId, socketId });
+  }
+};
+
+const removeUser = (socketId) => {
+  onlineUser = onlineUser.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return onlineUser.find((user) => user.userId === userId);
+};
+
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  socket.on('newUser', (userId) => {
+    addUser(userId, socket.id);
+
+    console.log(onlineUser);
+  });
+
+  socket.on('sendMessage', ({ receiverId, data }) => {
+    // console.log(receiverId, 'receiverId');
+    // console.log(data, 'data');
+    const receiver = getUser(receiverId);
+
+    io.to(receiver.socketId).emit('getMessage', data);
+  });
+
+  socket.on('disconnect', () => {
+    removeUser(socket.id);
+  });
+});
 
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
@@ -21,6 +70,8 @@ app.use('/api/test', testRouter);
 app.use('/api/chats', chatRoute);
 app.use('/api/messages', messageRoute);
 
-app.listen(8800, () => {
-  console.log('server is running!');
+server.listen(8800, () => {
+  console.log('Server + Socket.IO running on port 8800');
 });
+
+export { io };
