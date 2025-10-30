1cce4cd6afba023d824464629e17da3f364047ae
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2025-10-30 11:00:57 +0400

    fix/cors

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ io.on('connection', (socket) => {
 
 app.use(
   cors({
-    origin: ['https://lamaesate-front.vercel.app/', 'http://localhost:5173'],
+    origin: ['https://lamaesate-front.vercel.app/'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   })
